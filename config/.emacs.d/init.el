d2a7a001b8384c9ff32bd1e0e40f31159a8404ab
Jonathan Simpson <jsimpson.github@gmail.com>
2022-01-26 19:57:58 -0800

    doom-modeline-height doesnt appear to be doing anything...

--- config/.emacs.d/init.el
+++ config/.emacs.d/init.el
@@ -220,8 +220,7 @@
 (use-package all-the-icons)
 
 (use-package doom-modeline
-  :init (doom-modeline-mode 1)
-  :custom (doom-modeline-height 15))
+  :init (doom-modeline-mode 1))
 
 ;; start server
 (require 'server)
