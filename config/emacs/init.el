6f0e77e898bff35f7d47345a3dd76fbec745bdc0
Jonathan Simpson <jsimpson.github@gmail.com>
2022-01-31 12:26:29 -0800

    add elpa to package archives

--- config/emacs/init.el
+++ config/emacs/init.el
@@ -60,7 +60,8 @@
 ;; load package manager, add the MELP package repository
 (require 'package)
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
-                         ("org" . "https://orgmode.org/elpa/")))
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
 
 (package-initialize)
 (unless package-archive-contents
