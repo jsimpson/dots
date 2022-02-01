cde66f2b18161eef0ceb7360e722022b80538f09
Jonathan Simpson <jsimpson.github@gmail.com>
2022-02-01 13:18:34 -0800

    use company for completions

--- config/emacs/init.el
+++ config/emacs/init.el
@@ -144,6 +144,20 @@
   (add-hook 'before-save-hook #'lsp-format-buffer t t)
   (add-hook 'before-save-hook #'lsp-organize-imports t t)))
 
+; completions
+(use-package company
+  :after (lsp-mode)
+  :hook (lsp-mode . company-mode)
+  :bind
+  (:map company-active-map ("<tab>" . company-complete-selection))
+  (:map lsp-mode-map ("<tab>" . company-indent-or-complete-common))
+  :custom
+  (company-minimum-prefix-length 1)
+  (company-idle-delay 0.0))
+
+(use-package company-box
+  :hook (company-mode . company-box-mode))
+
 (use-package ivy
   :init (ivy-mode))
 (use-package swiper)
